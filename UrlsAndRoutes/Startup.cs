49e48ff528dccfffcb15b4f7f5aa45978409f0c5
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-04-25 16:09:16 +0700

    Ограничение маршрута специфическим набором значений переменной сегмента (Startup.cs).

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -36,6 +36,9 @@ namespace UrlsAndRoutes
             {
                 // Примененное к маршруту ограничение обеспечивает его соответствие только таким URL, в которых сегмент controller начинается с буквы H
                 endpoints.MapControllerRoute(name: "NewRoute", pattern: "{controller:regex(^H.*)=Home}/{action=Index}/{id?}");
+                // Ограничение соответствует только таким URL, в которых значение переменной controller начинается на букву H, а значением action
+                // являются Index или About.
+                endpoints.MapControllerRoute(name: "MeNewRoute2", pattern: "{controller:regex(^H.*)=Home}/"+"{action:regex(^Index$|^About$)=Index}/{id?}");
 
             });      
         }
