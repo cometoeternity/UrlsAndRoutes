7e37b5d4bfbbaff6c3e2b439cf4e82c4f2cd8c3d
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-04-25 17:27:03 +0700

    Определение специального ограничения (WeekDayConstraint.cs). Применение спецаильного ограничения простым образом и применение специального ограничения встраиваемым образом (Startup.cs)

---
+++ UrlsAndRoutes/Infrastructure/WeekDayConstraint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class WeekDayConstraint : IRouteConstraint
+    {
+        private static string[] Days = new[] { "mon", "tue", "wed", "thu", "fri", "sat", "sun" };
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return Days.Contains(values[routeKey]?.ToString().ToLowerInvariant());
+        }
+    }
+    // В интрейфейсе IRouteConstraint определен метод Match(), который вызывается, чтобы позволить ограничению решить, должен ли запрос соответствовать
+    // маршруту. Параметры метода Match() предоставляют доступ к запросу, поступившему от клиента, маршруту, имени ограничиваемого сегмента, переменных сегментов,
+    // извлеченных из URL, и признаку того, для какого URL проверяется запрос - входящего или исходящего.
+    // В примере с помощью параметра routeKey из параметра values извлекается значение переменной сегмента, к которому было применено ограничение, преобразуется
+    // в строку нижнего регистра и проверяется на предмет соответствия одному из дней недели, определенных в статическом поле Days.
+
+}
--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UrlsAndRoutes.Infrastructure;
 
 namespace UrlsAndRoutes
 {
@@ -17,6 +19,11 @@ namespace UrlsAndRoutes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // В методе ConfigureServices конфигурируется объект RouteOptions, который управляет рядом линий проведения системы маршрутизации.
+            // Свойство ConstraintMap возвращает словарь, используемый для трансляции имен встраиваемых ограничеий в классы реализации
+            // IRouteConstraint, которые предоставляют логику ограничений. В словарь было добавлено новое отображение, поэтому на класс
+            // WeekDayConstraint можно ссылаться встраевым образом как на weekday
+            services.Configure<RouteOptions>(options => options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
             services.AddMvc();
         }
 
@@ -34,8 +41,10 @@ namespace UrlsAndRoutes
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(name: "NewRoute", pattern: "{controller=Home}/{action=Index}/{id:range(10,20)}");
-                endpoints.MapControllerRoute(name: "MyNewRoute", pattern: "{controller=Home}/{action=Index}"+"/{id:alpha:minlength(6)?}");
+                endpoints.MapControllerRoute(name: "NewRoute", pattern: "{controller}/{action/{id?}",defaults: new { controller = "Home", action = "Index"}, 
+                    constraints: new { id = new WeekDayConstraint()});
+                // Практическое применение специального ограничения встраиваемым образом.
+                endpoints.MapControllerRoute(name: "NewRoute2", pattern: "{controller=Home}/{action=Index}/{id:weekday?}");
             });      
         }
     }
